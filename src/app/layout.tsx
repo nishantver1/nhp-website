2f241a8abfa3b710fbab110656e706802b54444d
nishantver1 <nishantverma7681@gmail.com>
2026-05-14 15:06:01 +0530

    metadatafix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import "./globals.css"
 
 export const metadata = {
   title: "Nishant Homeo pharmacy",
+   metadataBase: new URL("https://www.mynhp.in"),
   description: "Nishant Homeo Pharmacy in Burari Delhi offering homeopathic treatment for allergies, thyroid, hair fall, PCOS, skin problems and chronic diseases.",
     keywords: [
     "Nishant Homeo Pharmacy",
@@ -22,14 +23,46 @@ export const metadata = {
     "Homeopathy near me",
     "Best homeopathy clinic in Delhi",
   ],
+  openGraph: {
+    title: "Nishant Homeo Pharmacy",
+    description:
+      "Homeopathic treatment for allergies, thyroid, hair fall, PCOS, skin problems and chronic diseases.",
+    url: "https://www.mynhp.in/nhp-nobg.png",
+    siteName: "Nishant Homeo Pharmacy",
+    images: [
+      {
+        url: "./nhp-nobg.png",
+        width: 1200,
+        height: 630,
+        alt: "Nishant Homeo Pharmacy",
+      },
+    ],
+    locale: "en_IN",
+    type: "website",
+  },
 }
+
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  <script
+  type="application/ld+json"
+  dangerouslySetInnerHTML={{
+    __html: JSON.stringify({
+      "@context": "https://schema.org",
+      "@type": "MedicalClinic",
+      name: "Nishant Homeo Pharmacy",
+      url: "https://www.mynhp.in",
+      logo: "https://www.mynhp.in/nhp-nobg.png",
+      image: "https://www.mynhp.in/nhp-nobg.png",
+    }),
+  }}
+/>
   return (
+    
     <html lang="en">
       <body>
         <TopBar/>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,19 @@ import Hero from "../components/Hero"
 import LiveDoctorAvailabilityCalendar from "../components/LiveDoctorAvailabilityCalendar"
 
 export default function Home() {
+  <script
+  type="application/ld+json"
+  dangerouslySetInnerHTML={{
+    __html: JSON.stringify({
+      "@context": "https://schema.org",
+      "@type": "MedicalClinic",
+      name: "Nishant Homeo Pharmacy",
+      url: "https://www.mynhp.in",
+      logo: "https://www.mynhp.in/nhp-nobg.png",
+      image: "https://www.mynhp.in/nhp-nobg.png",
+    }),
+  }}
+/>
   return (
     <main>
       <Hero />
